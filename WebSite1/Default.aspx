693546f743642a5368d240aa650c3ae311a7ba6e
Michel <42797392+Misk77@users.noreply.github.com>
2019-10-07 10:25:27 +0200

    Add project files.

---
+++ WebSite1/Default.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            </br></br>
+
+            <center>
+                <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="XX-Large" ForeColor="Green" Text="Phone Book"></asp:Label>
+                </center>
+            </br></br>
+
+            <table class="table table-hover table-responsive">
+
+                <tr>
+                    <td>
+                        <asp:Label ID="lblName" runat="server" Text="Name"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtName" runat="server" MaxLength="50"></asp:TextBox>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label ID="lblContact" runat="server" Text="Contact No. "></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtContact" runat="server" MaxLength="10"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblLocation" runat="server" Text="Location"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtLocation" runat="server" MaxLength="50"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" Text="Add" CssClass="btn btn-default btn-success" />
+                    </td>
+                    <td>
+                        <asp:Button ID="btnUpdate" runat="server" Enabled="False" OnClick="btnUpdate_Click" Text="Update" class="btn btn-danger btn-default"/>
+                    </td>
+                </tr>
+
+            </table>
+        </div>
+        <asp:GridView ID="gridBook" runat="server" class="table table-hover table-responsive">
+            <Columns>
+                <asp:HyperLinkField DataNavigateUrlFields="Id" DataNavigateUrlFormatString="Default.aspx?id={0}&amp;action=1" HeaderText="Delete" Text="Delete">
+                    <ControlStyle ForeColor="Red" />
+                </asp:HyperLinkField>
+                <asp:HyperLinkField DataNavigateUrlFields="Id" DataNavigateUrlFormatString="Default.aspx?id={0}&amp;action=2" HeaderText="Edit" Text="Edit">
+                    <ControlStyle ForeColor="Green" />
+                </asp:HyperLinkField>
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebSite1/Default.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+    SqlCommand cmd;
+    SqlDataReader dr;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {          
+            if (Request.QueryString["id"] != null && Request.QueryString["action"] != null)
+            {
+                string id = Request.QueryString["id"];
+                string action = Request.QueryString["action"];
+                string name=null;
+                string contact=null;
+                string location=null;
+                con.Open();
+                cmd = new SqlCommand("select * from Phone_Book where Id='"+id+"'", con);
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    name = dr["Name"].ToString();
+                    contact = dr["Contact"].ToString();
+                    location = dr["Location"].ToString();
+                }
+                con.Close();
+                if (action == "1")
+                {
+                    con.Open();
+                    cmd = new SqlCommand("delete from Phone_Book where id='" + id + "'", con);
+                    int checkD = cmd.ExecuteNonQuery();
+                    if (checkD == 1)
+                    {
+                        ClientScript.RegisterStartupScript(this.GetType(), "deleted", "<script>alert('Contact Deleted...!!!');location='Default.aspx';</script>");
+                    }
+                    else
+                    {
+                        ClientScript.RegisterStartupScript(this.GetType(), "fail", "<script>alert('Failed...!!!');</script>");
+                    }
+                    con.Close();
+                }
+                else if (action == "2")
+                {
+                    btnAdd.Enabled = false;
+                    btnUpdate.Enabled = true;
+                    Session["id"] = id;
+                    txtName.Text = name;
+                    txtContact.Text = contact;
+                    txtLocation.Text = location;
+                }
+            }
+            con.Open();
+            cmd = new SqlCommand("select * from Phone_Book", con);
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                gridBook.DataSource = dr;
+                gridBook.DataBind();
+            }
+            con.Close();            
+        }
+    }
+
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        cmd = new SqlCommand("insert into Phone_Book values(@Name,@Contact,@Location)", con);
+        cmd.Parameters.Add("@Name", txtName.Text);
+        cmd.Parameters.Add("@Contact", txtContact.Text);
+        cmd.Parameters.Add("@Location", txtLocation.Text);
+        int count = cmd.ExecuteNonQuery();
+        if (count == 1)
+        {
+            ClientScript.RegisterStartupScript(this.GetType(), "added", "<script>alert('Contact Added...!!!');location='Default.aspx';</script>");
+            txtContact.Text = "";
+            txtLocation.Text = "";
+            txtName.Text = "";
+        }
+        else
+        {
+            ClientScript.RegisterStartupScript(this.GetType(), "failed", "<script>alert('Failed, please try again...!!!');</script>");
+        }
+        con.Close();
+    }
+
+    protected void btnUpdate_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        cmd = new SqlCommand("update Phone_Book set Name=@Name, Contact=@Contact, Location=@Location where id='" + Session["id"] + "'", con);
+        cmd.Parameters.Add("@Name", txtName.Text);
+        cmd.Parameters.Add("@Contact", txtContact.Text);
+        cmd.Parameters.Add("@Location", txtLocation.Text);
+        int checkD = cmd.ExecuteNonQuery();
+        if (checkD == 1)
+        {
+            ClientScript.RegisterStartupScript(this.GetType(), "updated", "<script>alert('Contact Updated...!!!');location='Default.aspx';</script>");
+            txtName.Text = "";
+            txtContact.Text = "";
+            txtLocation.Text = "";
+            btnAdd.Enabled = true;
+            btnUpdate.Enabled = false;
+            Session.Abandon();
+        }
+        else
+        {
+            ClientScript.RegisterStartupScript(this.GetType(), "Updatefail", "<script>alert('Failed...!!!');</script>");
+        }
+        con.Close();
+    }
+}
